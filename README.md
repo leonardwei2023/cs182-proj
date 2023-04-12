1c8603dc0d7ecc8005723ee688730869b18cd4bd
leonardwei2023 <57123326+leonardwei2023@users.noreply.github.com>
2023-04-12 13:11:51 -0700

    Updated ShapeNetDataset to return 3xN pyntcloud and updated README.md to include dataset usage

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# cs182-proj
+# PointNet
+
+## Installation
+- [Pytorch](https://pytorch.org/get-started/locally/)
+- [pyntcloud](https://pyntcloud.readthedocs.io/en/latest/installation.html)
+- pandas
+- ipywidgets
+- threejs
+- TODO: Add missing packages
+
+## Datasets
+- [ModelNet10](http://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip)
+- [ModelNet40](http://modelnet.cs.princeton.edu/ModelNet40.zip)
+
+## Files
+#### **dataset.py**
+```python
+# Load train/test dataset (3 X N)
+n = 10000
+train_data = ShapeNetDataset("datasets/ModelNet10", train=True, n=n)
+test_data = ShapeNetDataset("datasets/ModelNet10", train=False, n=n)
+
+# Create DataLoaders
+train_dataloader = DataLoader(train_data, batch_size=32, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=32, shuffle=True)
+
+# Getting points from point cloud
+train_data[10][0].points # Is in Pandas DataFrame
+
+# Visualizing PyntCloud (Only in notebook)
+train_data[10][0].plot(backend='threejs')
+```
--- dataset.py
+++ dataset.py
@@ -4,12 +4,13 @@ from torch.utils.data import Dataset
 from pyntcloud import PyntCloud
 
 class ShapeNetDataset(Dataset):
-    def __init__(self, root, train=True):
+    def __init__(self, root, train=True, n=10000):
         classes = [c for c in os.listdir(root) 
                    if os.path.isdir(os.path.join(root, c))]
         self.classes = {k: c for k, c in enumerate(classes)}
         self.path = root
         self.train = train
+        self.n = n
         self.df = pd.DataFrame()
         for label, c in self.classes.items():
             path = None
@@ -27,16 +28,20 @@ class ShapeNetDataset(Dataset):
         return len(self.df.index)
 
     def __getitem__(self, idx):
+        '''
+        Returns (PyntCloud, label)
+        '''
         path, label = self.df.loc[idx, 'path'], self.df.loc[idx, 'label']
         get_file = lambda p, t: PyntCloud.from_file(os.path.join(self.path, self.classes[label], t, path))
-        if self.train:
-            return get_file(path, 'train'), label
-        else:
-            return get_file(path, 'test'), label
+        test_train = 'train' if self.train else 'test'
+        return get_file(path, test_train).get_sample('mesh_random', n=self.n, as_PyntCloud=True), label        
 
 if __name__ == "__main__":
     train_dataset = ShapeNetDataset('datasets/ModelNet10', train=True)
     test_dataset = ShapeNetDataset('datasets/ModelNet10', train=False)
+
+    print(len(train_dataset))
+    print(len(test_dataset))
 
     print(train_dataset[10][0].points.boxplot())
     print(test_dataset[10][0].points.boxplot())
