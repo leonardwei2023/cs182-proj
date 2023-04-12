5c607b5692e3528eb2d4cec18d258e8d24a5fdd8
leonardwei2023 <57123326+leonardwei2023@users.noreply.github.com>
2023-04-11 22:41:28 -0700

    Created dataset loader and rough model

---
+++ dataset.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+from torch.utils.data import Dataset
+from pyntcloud import PyntCloud
+
+class ShapeNetDataset(Dataset):
+    def __init__(self, root, train=True):
+        classes = [c for c in os.listdir(root) 
+                   if os.path.isdir(os.path.join(root, c))]
+        self.classes = {k: c for k, c in enumerate(classes)}
+        self.path = root
+        self.train = train
+        self.df = pd.DataFrame()
+        for label, c in self.classes.items():
+            path = None
+            if self.train:
+                path = os.path.join(self.path, c, "train")
+            else:
+                path = os.path.join(self.path, c, "test")
+            dir_list = os.listdir(path)
+            label_list = [label]*len(dir_list)
+            df = pd.DataFrame(list(zip(dir_list, label_list)), columns=['path', 'label'])
+            self.df = pd.concat((self.df, df))
+        self.df.reset_index(inplace=True, drop=True)
+
+    def __len__(self):
+        return len(self.df.index)
+
+    def __getitem__(self, idx):
+        path, label = self.df.loc[idx, 'path'], self.df.loc[idx, 'label']
+        get_file = lambda p, t: PyntCloud.from_file(os.path.join(self.path, self.classes[label], t, path))
+        if self.train:
+            return get_file(path, 'train'), label
+        else:
+            return get_file(path, 'test'), label
+
+if __name__ == "__main__":
+    train_dataset = ShapeNetDataset('datasets/ModelNet10', train=True)
+    test_dataset = ShapeNetDataset('datasets/ModelNet10', train=False)
+
+    print(train_dataset[10][0].points.boxplot())
+    print(test_dataset[10][0].points.boxplot())
---
+++ model.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SharedMLP(nn.Sequential):
+    def __init__(self, layer_sizes):
+        layers = []
+        for i in range(len(layer_sizes)-1):
+            layers.append(nn.Conv1d(layer_sizes[i], layer_sizes[i+1], 1))
+        layers.append(nn.BatchNorm2d(layer_sizes[-1]))
+        layers.append(nn.ReLU())
+        super(SharedMLP, self).__init__(*layers)
+
+
+class T_net(nn.Module):
+    def __init__(self, size, dropout=0.7, bn_momentum=None):
+        super(T_net, self).__init__()
+        self.shared_mlp = SharedMLP([size, 64, 128, 1024])
+        self.fc1 = nn.Linear(1024, 512)
+        self.bn = nn.BatchNorm2d(512, momentum=bn_momentum)
+        self.fc2 = nn.Linear(512, 256)
+
+    def forward(self, x):
+        '''
+            Input: B x size x N
+        '''
+        out = self.shared_mlp(x)
+        out = F.relu(self.bn(self.fc1(out)))
+        out = self.fc2(out)
+        out = out.view(-1, 3, 3) + torch.eye(3).reshape((1,3,3)).repeat(x.size(0))
+        return out
+
+class InputTransform(nn.Module):
+    def __init__(self):
+        super(InputTransform, self).__init__()
+        self.T_net = T_net(3)
+
+    def forward(self, x):
+        out = self.T_net(x)
+        return torch.bmm(torch.transpose(input, 1, 2), out).transpose(1, 2)
+
+
+class FeatureTransform(nn.Module):
+    def __init__(self, size, reg=0.001):
+        super(FeatureTransform, self).__init__()
+        self.T_net = T_net(64)
+
+    def forward(self, x):
+        out = self.T_net(x)
+        # TODO: Add regulariation loss
+        return torch.bmm(torch.transpose(input, 1, 2), out).transpose(1, 2)
+
+class ClassficationNN(nn.Sequential):
+    def __init__(self, num_classes):
+        super(ClassficationNN, self).__init__()
+        self.input_transform = InputTransform()
+        self.mlp_1 = SharedMLP([3, 64, 64]),
+        self.feature_transform = FeatureTransform(),
+        self.mlp_2 = SharedMLP([64, 64, 128, 1024]),
+        self.mlp_3 = SharedMLP([1024, 512, 256, num_classes])
+
+    def forward(self, x):
+        out = self.input_transform(x)
+        out = self.mlp_1(out)
+        out = self.feature_transform(out)
+        out = self.mlp_2(out)
+        out = F.max_pool2d(out, x.size(2))
+        out = self.mlp_3(out)
+        return out
+
+# class SegmentationNN(nn.Sequential):
+#     def __init__(self, num_features: int):
+#         super(SegmentationNN, self).__init__()
+
+if __name__ == '__main__':
+    input_transform = T_net(3)
+    print('Input Transform:\n', input_transform)
+
+    feature_transform = T_net(64)
+    print('Feature Transform:\n', feature_transform)
+
+    
