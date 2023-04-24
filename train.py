bd62ff892ee97f95817e5806107ba1793c0a252e
Zixun <zixun@berkeley.edu>
2023-04-24 16:51:13 -0700

    updated auto data loading and parser

--- train.py
+++ train.py
@@ -8,13 +8,6 @@ import subprocess
 import os
 import argparse
 
-if os.path.exists('./datasets'):
-    pass
-else:
-    if not os.path.exists('./ModelNet10.zip'):
-        subprocess.run(["wget", "http://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip"])
-    subprocess.run(["unzip", "-o", "ModelNet10.zip", "-d", "./datasets/"])
-
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset_root', type=str, default = './datasets/ModelNet10', help='dataset root dir')
@@ -22,26 +15,43 @@ def parse():
     parser.add_argument('--batch_size', type=int, default = 32, help='batch size')
     parser.add_argument('--lr', type=float, default = 0.0001,  help='learning rate')
     parser.add_argument('--train_split', type=float, default = 0.7,  help='train/test split')
+    parser.add_argument('--point_num', type=int, default = 1000, help = 'point num of point cloud')
+    parser.add_argument('--cls_num', type=int, default = 10, help = 'class num')
+
 
     return parser.parse_args()
 
-def train_pointNet_cls():
-    opt = parse()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(device)
+def load_data(opt):
+    if os.path.exists(opt.dataset_root):
+        pass
+    else:
+        if opt.dataset_root=='./datasets/ModelNet10':
+            if not os.path.exists('./ModelNet10.zip'):
+                subprocess.run(["wget", "http://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip"])
+            subprocess.run(["unzip", "-o", "ModelNet10.zip", "-d", "./datasets/"])
+        if opt.dataset_root=='./datasets/ModelNet40':
+            if not os.path.exists('./ModelNet40.zip'):
+                subprocess.run(["wget", "http://modelnet.cs.princeton.edu/ModelNet40.zip"])
+            subprocess.run(["unzip", "-o", "ModelNet40.zip", "-d", "./datasets/"])
 
-    trainset = ShapeNetDataset(opt.dataset_root, train=True, n=10000)
+def prepare_dataset(opt):
+    trainset = ShapeNetDataset(opt.dataset_root, train=True, n=opt.point_num)
     dataset_length = len(trainset)
     train_len = int(dataset_length * opt.train_split)
     test_len = dataset_length - train_len
     trainset, testset = random_split(trainset, [train_len, test_len])
     testset.train = False
+    return trainset, testset
 
-    num_classes = 10
-    classification_model = ClassificationNN(num_classes).to(device)
+def train_pointNet_cls():
+    opt = parse()
+    load_data(opt)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    trainset, valset = prepare_dataset(opt)
+    classification_model = ClassificationNN(opt.cls_num).to(device)
 
     optimizer = torch.optim.Adam(classification_model.parameters(), lr=opt.lr)
-    train(classification_model, trainset, testset, optimizer, epochs=opt.epochs, batch_size=opt.batch_size, device=device)
+    train(classification_model, trainset, valset, optimizer, epochs=opt.epochs, batch_size=opt.batch_size, device=device)
 
 if __name__=='__main__':
     train_pointNet_cls()
